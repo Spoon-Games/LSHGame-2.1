c5df452fc7bc6539b7b31c5da5c91d076709bc20
SpoonGame <SpoonGames@web.de>
2020-10-20 16:55:29 +0200

    Edited Effect Triggers

---
+++ Assets/LSHGame/Util/Effects/EffectGroup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace LSHGame.Util
+{
+    [DisallowMultipleComponent]
+    public class EffectGroup : EffectsController, IEffectTrigger
+    {
+        public const string MATERIAL_ID = "Material";
+
+        [SerializeField]
+        private string currentMaterial = "";
+
+        public void Trigger(Bundle parameters)
+        {
+            if (string.IsNullOrEmpty(currentMaterial))
+            {
+                effectTriggers.Values.FirstOrDefault()?.Trigger(parameters);
+            }
+            else
+            {
+                base.Trigger(currentMaterial, parameters);
+            }
+        }
+
+        public void AddToDict(Dictionary<string, IEffectTrigger> triggers)
+        {
+            triggers.Add(name, this);
+        }
+
+        public void SetAttributes(Bundle values)
+        {
+            values.TryGet(MATERIAL_ID, out currentMaterial);
+        }
+    }
+}
