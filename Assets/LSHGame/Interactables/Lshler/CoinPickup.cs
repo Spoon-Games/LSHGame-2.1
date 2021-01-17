36db435346ef98c0fabb15597e62ed657045c0f5
Thomas Karb <tlowek15@web.de>
2021-01-17 17:53:28 +0100

    Added pickable LSHler

---
+++ Assets/LSHGame/Interactables/Lshler/CoinPickup.cs
@@ -0,0 +1,43 @@
+﻿using SceneM;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LSHGame.General.Environment
+{
+    public class CoinPickup : DataPersistBehaviour
+    {
+        [SerializeField]
+        private InventoryItem inventoryItem;
+
+        [SerializeField]
+        private UnityEvent onActivateEvent;
+
+        private bool activated = false;
+
+        public void Activate()
+        {
+            if (!activated)
+            {
+                onActivateEvent?.Invoke();
+                Inventory.Add(inventoryItem);
+                activated = true;
+                Destroy(gameObject);
+            }
+        }
+
+        public override void LoadData(SceneM.Data data)
+        {
+            if(data is Data<bool> storedData && storedData.value)
+            {
+                activated = true;
+                Destroy(gameObject);
+                //gameObject.SetActive(false);
+            }
+        }
+
+        public override SceneM.Data SaveData()
+        {
+            return new Data<bool>(activated);
+        }
+    } 
+}
